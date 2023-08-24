a8be965a82c0f235055fd47bc4af4365f68c8a30
AbhilashMadi <madiabhilash1563@gmail.com>
2023-08-24 23:55:04 +0530

    setup installation of shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import SCButton from './components/SCButton';
+
+function App() {
+
+  return (
+    <>
+      <SCButton />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/SCButton.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button';
+import { FC } from 'react';
+
+const SCButton:FC = () => {
+  return <Button>Click Me</Button>
+}
+
+export default SCButton;
