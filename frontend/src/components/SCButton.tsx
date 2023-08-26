20588d4e025ac79a2308803fa50ada073cc94893
AbhilashMadi <madiabhilash1563@gmail.com>
2023-08-26 23:56:16 +0530

    folder structure

--- frontend/src/components/SCButton.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from '@/components/ui/button';
-import { FC } from 'react';
-
-const SCButton:FC = () => {
-  return <Button>Click Me To render button</Button>
-}
-
-export default SCButton;
